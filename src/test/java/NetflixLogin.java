d6b609060411b00819b89aef545e6f670986af74
bilgek <pozanbilge@gmail.com>
2022-05-14 13:00:24 +0300

    format code

--- src/test/java/NetflixLogin.java
+++ src/test/java/NetflixLogin.java
@@ -5,24 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NetflixLogin {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.netflix.com/browse");
 
-        WebElement username=driver.findElement(By.id("id_userLoginId"));
+        WebElement username = driver.findElement(By.id("id_userLoginId"));
         username.sendKeys("example@example.com");
 
-        WebElement password=driver.findElement(By.id("id_password"));
+        WebElement password = driver.findElement(By.id("id_password"));
         password.sendKeys("12345");
 
         WebElement login = driver.findElement(By.xpath("//button[@data-uia='login-submit-button']"));
         login.click();
-
-
-
-
-
-
 
     }
 
